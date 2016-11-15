3a19ab4297b4ec6df8cf867fa1b532655972b68a
Administrator <Administrator@192.168.0.109>
2016-11-15 11:02:41 +0900

    MemberVO 추가

--- hjweb/src/model/MemberVO.java
+++ hjweb/src/model/MemberVO.java
@@ -1,5 +1,24 @@
 package model;
 
 public class MemberVO {
+	private String id;
 
+	public MemberVO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public MemberVO(String id) {
+		super();
+		this.id = id;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+	
 }
